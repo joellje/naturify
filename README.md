a42a35823e7cdc8405cce22d19b2e8041e5549e7
Joel Lim <joellimjieen@hotmail.com>
2023-09-05 15:23:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Naturify
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+Web App where users can make natural language queries to interact with Spotify
---
+++ lc/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About World!</h1>'
